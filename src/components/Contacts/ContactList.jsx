184d798740a0f227c74ba365efe67472d943de30
ViktorZvonar <viktorzvonar@yahoo.com>
2023-01-22 14:24:33 +0200

    propTypes fixes

--- src/components/Contacts/ContactList.jsx
+++ src/components/Contacts/ContactList.jsx
@@ -30,6 +30,12 @@ const ContactList = ({ contacts, onDeleteContact }) => {
 export default ContactList;
 
 ContactList.propTypes = {
-  contacts: PropTypes.string.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
   onDeleteContact: PropTypes.func.isRequired,
 };
